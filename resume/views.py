e93429d8e9b88a039021978f7b892ba872dbb661
Mmachi Ezeh <mmachilawri@gmail.com>
2025-02-13 10:06:02 +0100

    i worked on the about page and  project page

--- resume/views.py
+++ resume/views.py
@@ -3,3 +3,39 @@ from django.shortcuts import render
 # Create your views here.
 def home(request):
     return render (request, "home.html")
+
+def about(request):
+    return render (request, "about.html")
+
+def projects(request):
+    projects_show=[
+        {
+            "title": "Weather forecast",
+            "path": "images/weather.jpeg",
+           
+        },
+        {
+            "title": "Job application",
+            "path": "images/job.png",
+          
+        },
+        
+        {
+            "title": "Calculator",
+            "path": "images/calculator.png",    
+        },
+        
+        {
+            "title": "Student Management system",
+            "path": "images/management.png  ", 
+        },
+        {
+            "title": "Restaurant kitchen webapp",
+            "path": "images/kitchen.jpeg  ",  
+        },
+        {
+            "title": "Todo-list",
+            "path": "images/todo.jpeg",
+        },
+    ]
+    return render (request, "projects.html", {"projects_show": projects_show})
